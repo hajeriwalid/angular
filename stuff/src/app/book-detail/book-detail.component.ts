61c9e9fd5e46c8d32b79f0c625ff64bb6bb4bed5
whajeri <walidoo@gmail.com>
2017-05-25 09:59:35 +0100

    2nd angular commit

--- stuff/src/app/book-detail/book-detail.component.ts
+++ stuff/src/app/book-detail/book-detail.component.ts
@@ -3,12 +3,15 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Book } from './book';
 
 
+
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css']
 })
 
+
+//Always export the component class because you'll always import it elsewhere.
 export class BookDetailComponent implements OnInit {
 
   constructor() { }
@@ -17,5 +20,7 @@ export class BookDetailComponent implements OnInit {
   }
 
   @Input() book: Book;
+
+ // book: Book;
 
 }
--- stuff/src/app/list-books/list-books.component.ts
+++ stuff/src/app/list-books/list-books.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-//import { Book } from '../book-detail/book';
+import { Book } from '../book-detail/book';
+
+
+
 
 @Component({
   selector: 'app-list-books',
   templateUrl: './list-books.component.html',
   styleUrls: ['./list-books.component.css']
 })
+
+
 export class ListBooksComponent implements OnInit {
 
   title: String = "hello world from Listof Books";
-  listofBooks: any[] = ["Book one","Book two","Book three"]; 
+  //listofBooks: any[] = ["Book one","Book two","Book three"]; 
+
+  listofBooks: Book[] = [
+  { id: 11, name: 'Mr. Nice' , author: 'jean' },
+  { id: 12, name: 'Narco' , author: 'jean'},
+  { id: 13, name: 'Bombasto' , author: 'jean'},
+  { id: 14, name: 'Celeritas', author: 'jean' },
+  { id: 15, name: 'Magneta' , author: 'jean'},
+  { id: 16, name: 'RubberMan', author: 'renard' },
+  { id: 17, name: 'Dynama' , author: 'renard'},
+  { id: 18, name: 'Dr IQ', author: 'renard' },
+  { id: 19, name: 'Magma', author: 'renard' },
+  { id: 20, name: 'Tornado', author: 'renard' }
+];
+
+  selectedBook: Book;
+
+  onSelect(book: Book): void {
+    this.selectedBook = book;
+  }
+
   constructor() { }
 
   ngOnInit() {
